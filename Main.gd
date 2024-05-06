2a7924fce62b6574bde204b981e7e29b57d02643
Keroflux <49621236+Keroflux@users.noreply.github.com>
2024-05-06 20:59:40 +0200

    Lagt til interface og endret beregning

--- Main.gd
+++ Main.gd
@@ -3,14 +3,27 @@ extends Node2D
 var num_houses := 0
 var price := [496327.5, 380000.0]
 var used_price := 0
-var houses = {"15/11": [0.343, 2], 
+
+var houses = {"11": [0.343, 2],
+				"15": [0.343, 2], 
 				"6": [0.632, 1], 
-				"14/18": [0.665, 2], 
+				"14": [0.665, 2],
+				"18": [0.665, 2], 
 				"21": [0.742, 1], 
-				"Toppen": [1.0, 9], 
-				"Mest": [1.3, 0]}
+				"32": [1.0, 9],
+				"35": [1.0, 9],
+				"31": [1.0, 9],
+				"34": [1.0, 9],
+				"59": [1.0, 9],
+				"60": [1.0, 9],
+				"40": [1.0, 9],
+				"30": [1.0, 9],
+				"Dan-Egil": [1.0, 9]
+				}
+				
 var price_per_house := 0.0
-var ammont_to_split := 5000.0
+var ammont_to_split := 2000.0
+var tag = preload("res://HouseTag.tscn")
 
 func _ready():
 	init_price()
@@ -29,23 +42,29 @@ func calc():
 		unit_rest = rest / num_houses
 		for house in houses:
 			houses[house][2] += unit_rest * houses[house][0]
-			total += houses[house][2] * houses[house][1]
+			total += houses[house][2]
 		rest = price[used_price] - total
 	print(rest)
 	var the_last_penny = rest / num_houses
 	print(the_last_penny)
 	for house in houses:
 		houses[house][2] += the_last_penny
+		var a = tag.instantiate()
+		$ScrollContainer/VBoxContainer.add_child(a)
+		a.get_node("Name").text = house
+		a.get_node("Precent").text = str(houses[house][0])
+		a.get_node("Price").text = str(snapped(houses[house][2], 0.01))
 	print(houses)
 	var tot := 0.0
 	for house in houses:
-		tot += houses[house][2] * houses[house][1]
+		tot += houses[house][2]
 	print(tot)
 
 
 func init_price():
+	num_houses = 0
 	for key in houses:
-		num_houses += houses[key][1]
+		num_houses += 1
 	price_per_house = price[used_price] / num_houses
 	for key in houses:
 		houses[key].append(price_per_house * houses[key][0])
